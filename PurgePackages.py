872040b972f2d42996986f4512b326b4aaceecbe
Vasily Zakharov <vmzakhar@gmail.com>
2020-10-20 21:33:33 +0300

    Added PurgePackages.py

---
+++ PurgePackages.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+from re import compile as reCompile
+from subprocess import Popen, PIPE, STDOUT
+from sys import exit as sysExit
+from typing import Callable, Dict, List, Optional, Sequence
+
+LIST_PACKAGE_PATTERN = reCompile(r'(?m)^(?P<package>[^/]+)/(?P<date>[^ ]+) (?P<version>[^ ]+) (?P<arch>[^ ]+) (?P<status>\[installed,local\])$')
+
+PURGE_PACKAGE_PATTERN = reCompile(r'(?ms).*The following packages will be REMOVED:\n(?P<packages>(?:  [^\n]*\n)+)')
+
+PACKAGE_PATTERN = reCompile(r'[^\s*]+')
+
+PURGE_EXCLUDE_PATTERN = reCompile(r'Note, selecting|is not installed, so not removed')
+
+def purgeFilter(line: str) -> Optional[str]:
+    if line.endswith(' disk space will be freed.\n'):
+        return line + 'Do you want to continue? [Y/n] ' # Add the question that gets suppressed by subprocess buffering
+    return line
+
+def runProcess(args: Sequence[str], lineFilter: Optional[Callable[[str], Optional[str]]] = None) -> str:
+    print('$', ' '.join(args))
+    subProcess = Popen(args, stdout = PIPE, stderr = STDOUT, bufsize = 0)
+    if lineFilter:
+        output = []
+        assert subProcess.stdout is not None
+        for byteLine in subProcess.stdout:
+            line = lineFilter(byteLine.decode())
+            if line is None:
+                continue
+            output.append(line)
+            print(line, end = '', flush = True)
+        ret = ''.join(output + ['',])
+        (out, err) = subProcess.communicate()
+        assert not out, f"Unexpected output: {out.decode()!r}"
+    else:
+        (out, err) = subProcess.communicate()
+        ret = out.decode()
+    assert not err, f"Unexpected error output: {err.decode()!r}"
+    if subProcess.returncode:
+        raise Exception(f"Unexpected return code {subProcess.returncode}")
+    return ret
+
+def main() -> None:
+    try:
+        print("\n## Checking [installed,local] packages...\n")
+        packages: List[str] = []
+        for match in LIST_PACKAGE_PATTERN.finditer(runProcess(('apt', 'list', '--installed'))):
+            package = match.groupdict()['package']
+            assert package
+            print(package)
+            packages.append(package)
+        packages.sort()
+        if not packages:
+            print("No local installed packages found!")
+            return
+        print("\n## Checking dependencies:\n")
+        dependencies: Dict[str, List[str]] = {}
+        for package in packages:
+            m = PURGE_PACKAGE_PATTERN.match(runProcess(('apt-get', '-s', 'remove', package)))
+            if not m:
+                print(f"{package}: Error retrieving dependencies")
+                continue
+            packagesToPurge = PACKAGE_PATTERN.findall(m.groupdict()['packages'])
+            packagesToPurge.remove(package)
+            packagesToPurge.sort()
+            print(f"{package}: {' '.join(packagesToPurge) or '-'}")
+            dependencies[package] = packagesToPurge
+        changed = True
+        while changed:
+            changed = False
+            for (package, d) in dependencies.items():
+                if d:
+                    for p in d[:]:
+                        if not dependencies[p]:
+                            d.remove(p)
+                            changed = True
+        print(f"\n## Can NOT be purged: {' '.join(p for (p, d) in dependencies.items() if d) or 'None'}")
+        toPurge = tuple(p for (p, d) in dependencies.items() if not d)
+        print(f"\n## Can be purged: {' '.join(toPurge) or 'None'}")
+        if toPurge:
+            runProcess(('sudo', 'apt-get', 'purge') + toPurge, lineFilter = purgeFilter)
+    except Exception as e:
+        print(f"ERROR! {e}")
+        sysExit(1)
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -10,3 +10,18 @@ Usage:
 
 No options are supported and none is needed.
 
+# PurgePackages
+Python3 script to purge obsolete packages from Ubuntu OS.
+
+After Ubuntu gets upgraded to the next release, some packages from old distribution can get stuck.
+They're installed, but don't have any repositories associated with them, so they never get updated.
+
+These packages can be an unneeded waste of disk space,
+they can produce frustration when they don't work as expected on a newer distribution,
+or can provide a security gap as new discovered vulnerabilities do not get fixed.
+
+This script locates packages not associated with repositories and not having dependencies from them,
+and suggests removing them.
+
+Usage:
+`python3 PurgePackages.py`
